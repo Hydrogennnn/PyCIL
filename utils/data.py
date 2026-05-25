eec65652863dda79ad5ef452ee40af3f394c6fae
wjd <2902704036@qq.com>
2026-05-25 13:34:00 +0000

    写了部分MMEA

--- utils/data.py
+++ utils/data.py
@@ -6,6 +6,10 @@ from . import autoaugment
 from . import ops
 from sklearn.model_selection import train_test_split
 
+from .transforms import ArrayToTensor, DataStack, GroupNormalize, IdentityTransform, ImgStack, ToTorchFormatTensor, GroupScale, GroupCenterCrop
+import torch
+
+
 class iData(object):
     train_trsf = []
     test_trsf = []
@@ -167,3 +171,156 @@ class AVE(iData):
         self.test_targets = self.all_targets[self.test_data_idx]
         self.val_targets = self.all_targets[self.val_data_idx]
 
+
+
+
+class MMEA_CL(iData):
+    class_order = [
+        26,
+        14,
+        23,
+        4,
+        11,
+        25,
+        31,
+        10,
+        29,
+        5,
+        6,
+        9,
+        17,
+        22,
+        2,
+        19,
+        13,
+        1,
+        21,
+        16,
+        8,
+        3,
+        27,
+        28,
+        15,
+        30,
+        0,
+        7,
+        12,
+        18,
+        20,
+        24
+    ]
+
+
+    def __init__(self, model, train_list, test_list):
+        self.modality = ["RGB", "Gyro", "Acce"]
+        self.arch = "BNInception"
+        self.train_list = train_list
+        self.test_list = test_list
+        
+        self.crop_size = model.feature_extract_network.crop_size
+        self.scale_size = model.feature_extract_network.scale_size
+        self.input_mean = model.feature_extract_network.input_mean
+        self.input_std = model.feature_extract_network.input_std
+        self.data_length = model.feature_extract_network.new_length
+        self.train_augmentation = model.feature_extract_network.get_augmentation()
+
+        self.train_trsf = {}
+        self.test_trsf = {}
+        self.normalize = {}
+
+
+        
+    def download_data(self):
+        
+        for m in self.modality:
+            if (m != 'RGBDiff'):
+                self.normalize[m] = GroupNormalize(self.input_mean[m], self.input_std[m])
+            else:
+                self.normalize[m] = IdentityTransform()
+
+        for m in self.modality:
+            if (m != 'Gyro' and m != 'Acce'):
+                # Prepare train/val dictionaries containing the transformations
+                # (augmentation+normalization)
+                # for each modality
+                self.train_trsf[m] = transforms.Compose([
+                self.train_augmentation[m],
+                ImgStack(roll=self.arch == 'BNInception'),
+                ToTorchFormatTensor(div=self.arch != 'BNInception'),
+                self.normalize[m],
+                ])
+
+                self.test_trsf[m] = transforms.Compose([
+                    GroupScale(int(self.scale_size[m])),
+                    GroupCenterCrop(self.crop_size[m]),
+                    ImgStack(roll=self.arch == 'BNInception'),
+                    ToTorchFormatTensor(div=self.arch != 'BNInception'),
+                    self.normalize[m],
+                ])
+            else:
+                self.train_trsf[m] = transforms.Compose([
+                    DataStack(),
+                    ArrayToTensor(),
+                    self.normalize[m],
+                ])
+
+                self.test_trsf[m] = transforms.Compose([
+                    DataStack(),
+                    ArrayToTensor(),
+                    self.normalize[m],
+                ])
+            
+
+        train_set = MMEADataSet(self.train_list)
+        test_set = MMEADataSet(self.test_list)
+
+        self.train_data, self.test_data = np.array(train_set.video_list), np.array(test_set.video_list)
+        self.train_targets, self.test_targets = np.array(self._get_targets(train_set)), np.array(self._get_targets(test_set))
+
+    def _get_targets(self, dataset):
+        """
+        get target list from MyDataset
+        """
+        targets = []
+        for i in range(len(dataset)):
+            targets.append(dataset.video_list[i].label)
+
+        return targets
+
+class MMEADataSet(torch.utils.data.Dataset):
+    def __init__(self, list_file):
+        self.list_file = list_file
+
+        self._parse_list()
+
+        class MyDataset_VideoRecord():
+
+            def __init__(self, row):
+                self.data = row
+
+            @property
+            def path(self):
+                return self.data[0]
+
+            @property
+            def num_frames(self):
+                return {'RGB': int(self.data[1]),
+                        'Flow': int(self.data[1])-1,
+                        'Gyro': int(self.data[2]),
+                        'Acce': int(self.data[2])}
+
+            @property
+            def label(self):
+                return int(self.data[3])
+
+        self.MyDataset_VideoRecord = MyDataset_VideoRecord
+
+    def _parse_list(self):
+        
+        tmp = [x.strip().split(' ') for x in open(self.list_file)]
+        tmp = [item for item in tmp if int(item[1]) >= 3]
+        self.video_list = [self.MyDataset_VideoRecord(item) for item in tmp]
+        print('video number:%d' % (len(self.video_list)))
+
+    def __len__(self):
+        return len(self.video_list)
--- utils/data_manager.py
+++ utils/data_manager.py
@@ -7,6 +7,8 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 from utils.data import iCIFAR10, iCIFAR100, iImageNet100, iImageNet1000, iCIFAR10_AA, iCIFAR100_AA, AVE
 from tqdm import tqdm
+import os
+from scipy import signal
 import torch
 
 class DataManager(object):
@@ -290,6 +292,219 @@ class AVE_DummyDataset(DummyDataset):
             sample, _ = self.__getitem__(idx)
             data.append(sample)
         return data
+
+class MMEA_DummyDataset(DummyDataset):
+    def __init__(
+        self,
+        video_list,
+        labels,
+        trsf,
+        use_path=False,
+        aug=1,
+        appendent_data=None,
+    ):
+        super().__init__(video_list, labels, trsf, use_path, aug, appendent_data)
+        self.video_list = self.data
+        self.m = 3
+    
+    def _mpu_data_convert(self, ori_mpu_data, acc_sensitivity=8192, gyro_sensitivity=16.4):
+
+        acc_x = ori_mpu_data[0] / acc_sensitivity
+        acc_y = ori_mpu_data[1] / acc_sensitivity
+        acc_z = ori_mpu_data[2] / acc_sensitivity
+
+        gyro_x = ori_mpu_data[3] / gyro_sensitivity
+        gyro_y = ori_mpu_data[4] / gyro_sensitivity
+        gyro_z = ori_mpu_data[5] / gyro_sensitivity
+
+        return [acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z]
+
+    def _median_filter(self, true_mpu_datas, kernel_size=3):
+        filter_datas = []
+        for i in range(6):
+            filter_data = signal.medfilt(true_mpu_datas[:, i], kernel_size=kernel_size)
+            filter_datas.append(filter_data)
+
+        return np.array(filter_datas).astype(np.float32)
+
+    def _trapz(self, filter_datas):
+        mean = np.mean(filter_datas, axis=1).reshape((3, 1))
+        filter_datas = filter_datas - mean
+        angles = []
+        init_angle = filter_datas[:, 0].reshape((1, 3))
+        for i in range(1, filter_datas.shape[1]+1):
+            angle = trapz(filter_datas[:, 0:i])
+            angles.append(angle)
+        angles = np.array(angles) + init_angle
+        
+        return angles.astype(np.float32)
+    
+    def _mpu_process(self, record):
+        file_path = record.path.replace('/home/amax/Downloads/whx/temporal-binding-network/dataset/data/', self.mpu_path) + '.csv'
+        try:
+            mpu_datas = pd.read_csv(file_path, header=None)
+            true_mpu_datas = []
+            for i in range(len(mpu_datas)):
+                ori_data = mpu_datas.loc[i]
+                true_mpu_data = self._mpu_data_convert(ori_data)
+                true_mpu_datas.append(true_mpu_data)
+
+            filter_datas = self._median_filter(np.array(true_mpu_datas), kernel_size=5)
+            angles = self._trapz(filter_datas[3:6, :])
+
+            self._process_acce_data = filter_datas[0:3, :].T
+            self._process_gyro_data = angles
+        except Exception:
+            print('error loading gyro file:', file_path)    
+    
+    def _load_data(self, modality, record, idx):
+        if modality == 'RGB' or modality == 'RGBDiff':
+            try:
+                return [Image.open(os.path.join(record.path, self.image_tmpl[modality].format(idx))).convert('RGB')]
+            except Exception:
+                print('error loading image:', os.path.join(record.path, self.image_tmpl[modality].format(idx)))
+
+        elif modality == 'Flow':
+            try:
+                x_img = Image.open(os.path.join(record.path, self.image_tmpl[modality].format('x', idx))).convert('L')
+                y_img = Image.open(os.path.join(record.path, self.image_tmpl[modality].format('y', idx))).convert('L')
+                return [x_img, y_img]
+            except Exception:
+                print('error loading flow image:', os.path.join(record.path, self.image_tmpl[modality].format('x/y', idx)))
+
+        elif modality == 'Acce':
+            file_path = record.path.replace('/home/amax/Downloads/whx/temporal-binding-network/dataset/data/', self.mpu_path) + '.csv'
+            try:
+                mpu_data = self._process_acce_data[idx]
+                return mpu_data
+            except IndexError:
+                print('error loading gyro file:', file_path, idx)
+
+        elif modality == 'Gyro':
+            file_path = record.path.replace('/home/amax/Downloads/whx/temporal-binding-network/dataset/data/', self.mpu_path) + '.csv'
+            try:
+                mpu_data = self._process_gyro_data[idx]
+                return mpu_data
+            except IndexError:
+                print('error loading gyro file:', file_path, idx)
+
+    def _sample_indices(self, record, modality):
+        """
+
+        :param record: VideoRecord
+        :return: list
+        """
+        average_duration = (record.num_frames[modality] - self.new_length[modality] + 1) // self.num_segments
+        if average_duration > 0:
+            offsets = np.multiply(list(range(self.num_segments)), average_duration) + randint(average_duration, size=self.num_segments)
+        # elif record.num_frames[modality] > self.num_segments:
+        #     offsets = np.sort(randint(record.num_frames[modality] - self.new_length[modality] + 1, size=self.num_segments))
+        else:
+            offsets = np.zeros((self.num_segments,))
+
+        return offsets
+
+    def _get_val_indices(self, record, modality):
+        if record.num_frames[modality] > self.num_segments + self.new_length[modality] - 1:
+            tick = (record.num_frames[modality] - self.new_length[modality] + 1) / float(self.num_segments)
+            offsets = np.array([int(tick / 2.0 + tick * x) for x in range(self.num_segments)])
+        else:
+            offsets = np.zeros((self.num_segments,))
+
+        return offsets
+
+    def _get_test_indices(self, record, modality):
+
+        tick = (record.num_frames[modality] - self.new_length[modality] + 1) / float(self.num_segments)
+
+        offsets = np.array([int(tick / 2.0 + tick * x) for x in range(self.num_segments)])
+
+        return offsets
+
+    def __getitem__(self, index):
+
+        input = {}
+        record = self.video_list[index]
+
+        if 'Gyro' in self.modality or 'Acce' in self.modality:
+            self._mpu_process(record)
+
+        for m in self.modality:
+            if self.mode == 'train':
+                segment_indices = self._sample_indices(record, m)
+            elif self.mode == 'val':
+                segment_indices = self._get_val_indices(record, m)
+            elif self.mode == 'test':
+                segment_indices = self._get_test_indices(record, m)
+
+            # We implement a Temporal Binding Window (TBW) with size same as the action's length by:
+            #   1. Selecting different random indices (timestamps) for each modality within segments
+            #      (this is similar to using a TBW with size same as the segment's size)
+            #   2. Shuffling randomly the segments of Flow, Audio (RGB is the anchor hence not shuffled)
+            #      which binds data across segments, hence making the TBW same in size as the action.
+            #   Example of an action with 90 frames across all modalities:
+            #    1. Synchronous selection of indices per segment:
+            #       RGB: [12, 41, 80], Flow: [12, 41, 80], Audio: [12, 41, 80]
+            #    2. Asynchronous selection of indices per segment:
+            #       RGB: [12, 41, 80], Flow: [9, 55, 88], Audio: [20, 33, 67]
+            #    3. Asynchronous selection of indices per action:
+            #       RGB: [12, 41, 80], Flow: [88, 55, 9], Audio: [67, 20, 33]
+
+            if m != 'RGB' and self.mode == 'train':
+                np.random.shuffle(segment_indices)
+
+            img= self.get(m, record, segment_indices)
+            input[m] = img
+
+        return index, input, self.labels[index]
+
+    def get(self, modality, record, indices):
+
+        if modality != 'Gyro' and modality != 'Acce':
+            images = list()
+            for seg_ind in indices:
+                p = int(seg_ind)
+                for i in range(self.new_length[modality]):
+                    seg_imgs = self._load_data(modality, record, p)
+                    images.extend(seg_imgs)
+                    if p < record.num_frames[modality]:
+                        p += 1
+            process_data = self.transform[modality](images)
+        
+        elif modality == 'Acce':
+            mpu_datas1 = []
+            for seg_ind in indices:
+                mpu_data1 = []
+                p = int(seg_ind)
+                for i in range(self.new_length[modality]):
+                    single_mpu_data1 = self._load_data(modality, record, p)
+                    mpu_data1.append(single_mpu_data1)
+                    if p < record.num_frames[modality]:
+                        p += 1
+                
+                mpu_datas1.append(np.array(mpu_data1))
+
+            process_data = self.transform[modality](mpu_datas1)
+        
+        elif modality == 'Gyro':
+            mpu_datas2 = []
+            for seg_ind in indices:
+                mpu_data2 = []
+                p = int(seg_ind)
+                for i in range(self.new_length[modality]):
+                    single_mpu_data2 = self._load_data(modality, record, p)
+                    mpu_data2.append(single_mpu_data2)
+                    if p < record.num_frames[modality]:
+                        p += 1
+
+                mpu_datas2.append(np.array(mpu_data2))
+
+            process_data = self.transform[modality](mpu_datas2)
+
+        return process_data
+
+    def __len__(self):
+        return len(self.video_list)
 
 
 def _map_new_class_index(y, order):
